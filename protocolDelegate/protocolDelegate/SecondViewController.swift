e5bbcb46b7a926c3d4f5b7eafd9d52f4c149304b
ssarathe-systango <ssarathe@systango.com>
2022-07-20 15:34:03 +0530

    alert added in protocolDelegate

--- protocolDelegate/protocolDelegate/SecondViewController.swift
+++ protocolDelegate/protocolDelegate/SecondViewController.swift
@@ -12,6 +12,7 @@ protocol DataPass {
 
 class SecondViewController: UIViewController {
 
+    //MARK: Outlets
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var txtAddress: UITextField!
     @IBOutlet weak var txtCity: UITextField!
@@ -23,12 +24,31 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    //MARK: Action
     @IBAction func saveBtn(_ sender: Any) {
         
-        delegate.dataPassing(name: txtName.text ?? "", address: txtAddress.text ?? "", city: txtCity.text ?? "")
-        
+        if txtName.text == "" {
+            alert()
+        }
+        else if txtAddress.text == "" {
+            alert()
+        }
+        else if txtCity.text == "" {
+            alert()
+        }
+        else {
+            delegate.dataPassing(name: txtName.text ?? "", address: txtAddress.text ?? "", city: txtCity.text ?? "")
+        }
         txtName.text = ""
         txtAddress.text = ""
         txtCity.text = ""
+    }
+    
+    //MARK: Alert
+    func alert() {
+        let alert = UIAlertController(title: "Alert!", message: "Enter all values", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
