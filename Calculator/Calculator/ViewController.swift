aa64765bfb519d0ddc6190012f4f3b2efb602a30
ssarathe-systango <ssarathe@systango.com>
2022-07-22 14:51:31 +0530

    Calculator UI Updated

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -17,29 +17,77 @@ class ViewController: UIViewController {
     //MARK: Outlet
     @IBOutlet weak var label: UILabel!
     
+    // Operation Buttons Corner Style
+    @IBOutlet weak var cornerRadius1: UIButton!
+    @IBOutlet weak var backCornerRadius: UIButton!
+    @IBOutlet weak var modulusCornerRadius: UIButton!
+    @IBOutlet weak var cornerRadius2: UIButton!
+    @IBOutlet weak var cornerRadius3: UIButton!
+    @IBOutlet weak var cornerRadius4: UIButton!
+    @IBOutlet weak var cornerRadius5: UIButton!
+    @IBOutlet weak var cornerRadius6: UIButton!
+    @IBOutlet weak var pointCornerRadius: UIButton!
+    
+    @IBOutlet weak var cornerRadius7: UIButton!
+    
+    // Numbers Buttons Corner Style
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cornerRadius1.layer.cornerRadius = 37
+        backCornerRadius.layer.cornerRadius = 37
+        modulusCornerRadius.layer.cornerRadius = 37
+        cornerRadius2.layer.cornerRadius = 37
+        cornerRadius3.layer.cornerRadius = 37
+        cornerRadius4.layer.cornerRadius = 37
+        cornerRadius5.layer.cornerRadius = 37
+        cornerRadius6.layer.cornerRadius = 37
+        pointCornerRadius.layer.cornerRadius = 37
+        cornerRadius7.layer.cornerRadius = 37
+        
+        number1.layer.cornerRadius = 37
+        number2.layer.cornerRadius = 37
+        number3.layer.cornerRadius = 37
+        number4.layer.cornerRadius = 37
+        number5.layer.cornerRadius = 37
+        number6.layer.cornerRadius = 37
+        number7.layer.cornerRadius = 37
+        number8.layer.cornerRadius = 37
+        number9.layer.cornerRadius = 37
+        
     }
     
-    // MARK: Actions
-    
-    // action of numeric values buttons
+//  MARK: Action of numeric values
+
     @IBAction func numbers(_ sender: UIButton) {
         if performingMath == true {
-            print("Performing math True!")
-            // o/p :- single value like:- 1 or 2 or 3 etc...
+//            print("Performing math True!")
+//            o/p :- single value like:- 1 or 2 or 3 etc...
             label.text = String(sender.tag - 1)
             numberOnScreen = Double(label.text!)!
             performingMath = false
         } else {
-            print("Performing math False!")
-            // o/p :- string concatinate :- 12345 or 56789
+//            print("Performing math False!")
+//            o/p :- string concatinate :- 12345 or 56789
             label.text = label.text! + String(sender.tag - 1)
             numberOnScreen = Double(label.text!)!
         }
     }
     
-    // action for performing operations buttons
+//  MARK: Action of operations values
+
     @IBAction func Calculate(_ sender: UIButton) {
         if label.text != "" && sender.tag != 11 && sender.tag != 16 {
             
@@ -60,8 +108,20 @@ class ViewController: UIViewController {
             {
                 label.text = "+"
             }
+            else if sender.tag == 17
+            {
+                label.text = nil
+            }
+            else if sender.tag == 18
+            {
+                label.text = "%"
+            }
+            else if sender.tag == 19
+            {
+                label.text = "."
+            }
             
-            // operation store the tag value, according to user press the button ex : + = 15, * = 13 etc.
+//          operation store the tag value, according to user press the button ex : + = 15, * = 13 etc.
             operation = sender.tag
             performingMath = true
             
@@ -80,6 +140,12 @@ class ViewController: UIViewController {
             }
             else if operation == 15 {
                 label.text = String(previousNumber + numberOnScreen)
+            }
+            else if operation == 18 {
+                label.text = String(previousNumber * numberOnScreen)
+            }
+            else if operation == 19 {
+                
             }
         }
         
